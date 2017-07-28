c7e2bb3cfec9ea55ead525d59a6ee9c43b48a453
dan <dtmoodie@gmail.com>
2017-07-28 11:04:50 -0400

    compile time string to integer

--- include/ct/String.hpp
+++ include/ct/String.hpp
@@ -4,38 +4,53 @@
 namespace ct
 {
     template<size_t N>
-    constexpr size_t strLen(const char(&str)[N])
-    {
+    constexpr size_t strLen(const char(&str)[N]){
         return N;
     }
 
-    template<size_t N>
-    constexpr size_t findFirst(const char(&str)[N], char character = ':')
-    {
-        return detail::findFirst(str, N, 0, character);
+    constexpr size_t strLen(const char* str, size_t len = 0){
+        return *str ? strLen(str + 1, len + 1) : len;
     }
 
     template<size_t N>
-    constexpr size_t findLast(const char(&str)[N], char character = ':')
-    {
+    constexpr size_t findFirst(const char(&str)[N], char character = ':'){
+        return detail::findFirst(str, N, 0, character);
+    }
+
+    constexpr size_t findFirst(const char* str, char character = ':', size_t pos = 0){
+        return *str ? (*str == character) ? pos + 1 : findFirst(str + 1, character, pos+1) : pos;
+    }
+
+    constexpr size_t countOccurances(const char* str, char character = ':', size_t count = 0){
+        return *str ? countOccurances(str + 1, character,  *str == character ? count + 1 : count) : count;
+    }
+
+    template<size_t N>
+    constexpr size_t findLast(const char(&str)[N], char character = ':'){
         return detail::ctFindDeliminator(str, N - 1, character);
     }
 
     template<size_t Tlen>
-    constexpr size_t classNameIdx(const char(&str)[Tlen])
-    {
+    constexpr size_t classNameIdx(const char(&str)[Tlen]){
         return detail::ctFindDeliminator(str, Tlen - 1);
     }
     
     template<size_t Tlen>
-    constexpr uint32_t hashClassName(const char(&str)[Tlen])
-    {
+    constexpr uint32_t hashClassName(const char(&str)[Tlen]){
         return ctcrc32Range(str, classNameIdx(str));
     }
 
     // https://stackoverflow.com/questions/25195176/how-do-i-convert-a-c-string-to-a-int-at-compile-time
     constexpr bool isDigit(char c) {
         return c <= '9' && c >= '0';
+    }
+
+    constexpr int stoiImplRange(const char* str, const char* end, int value = 0){
+        return str != end ?
+            isDigit(*str) ?
+            stoiImplRange(str + 1, end, (*str - '0') + value * 10)
+            : throw "compile-time-error: not a digit"
+            : value;
     }
 
     constexpr int stoiImpl(const char* str, int value = 0) {
@@ -48,6 +63,13 @@ namespace ct
 
     constexpr int stoi(const char* str) {
         return stoiImpl(str);
+    }
+
+    constexpr int stoiRange(const char* start, const char* end){
+        return stoiImplRange(start, end);
+    }
+    constexpr int stoiRange(const char* start, int len) {
+        return stoiImplRange(start, start + len);
     }
 
 #define DECLARE_CLASS_HASH \
