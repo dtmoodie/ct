704028638369414bb05e700dcfc5ad55c5f0aa2f
dan <dtmoodie@gmail.com>
2018-07-21 13:58:35 -0400

    print cleanup

--- tests/reflect/common.hpp
+++ tests/reflect/common.hpp
@@ -5,12 +5,38 @@
 template<class Tester>
 void testTypes(Tester& tester)
 {
-    //tester.test<ReflectedStruct>();
-    //tester.test<Inherited>();
-    //tester.test<Composite>();
-    tester.template test<TestA>();
-    tester.template test<TestB>();
-    tester.template test<TestC>();
-    //tester.test<TestVec>();
-    //tester.test<InternallyReflected>();
+    {
+        ReflectedStruct data{ 0,1,2,3 };
+        tester.test(data);
+    }
+    {
+        Inherited data;
+        data.id = 0;
+        data.w = 0;
+        data.x = 0;
+        data.y = 1;
+        data.z = 2;
+        tester.test(data);
+    }
+    {
+        Composite data{ {0,1,2,3}, {4,5,6,7} };
+        tester.test(data);
+    }
+    {
+        TestA data{ 0,1,2 };
+        tester.test(data);
+    }
+    {
+        TestB data{ 0,1,2 };
+        tester.test(data);
+    }
+    {
+        TestC data{ 0,1,2 };
+        tester.test(data);
+    }
+    {
+        TestVec data;
+        data.vec = { 0,1,2,3,4 };
+        tester.test(data);
+    }
 }
--- tests/reflect/print.cpp
+++ tests/reflect/print.cpp
@@ -3,13 +3,32 @@
 #include <ct/reflect/print.hpp>
 #include <iostream>
 
+namespace std
+{
+    template<class T>
+    ostream& operator << (ostream& os, const std::vector<T>& vec)
+    {
+        if (!vec.empty())
+        {
+            os << '[';
+            for (size_t i = 0; i < vec.size(); ++i)
+            {
+                if (i != 0)
+                    os << ' ';
+                os << vec[i];
+            }
+            os << ']';
+        }
+        return os;
+    }
+}
+
 struct Printer
 {
     template<class T>
-    void test()
+    void test(T& data)
     {
-        T data;
-        std::cout << "====================\n";
+        std::cout << "\n====================\n";
         std::cout << ct::Reflect<T>::getName() << std::endl;
         ct::printStruct(std::cout, data);
         std::cout << std::endl;
