bebb7811f88772b242362c50125221915743de1d
dan <dtmoodie@gmail.com>
2018-07-21 12:11:02 -0400

    added a longer value for hash test

--- tests/hash/string_hash.cpp
+++ tests/hash/string_hash.cpp
@@ -10,4 +10,6 @@ int main()
     STATIC_EQUAL(ct::ctcrc32Range("asdfasdf", 4, 4), ct::ctcrc32(getString()));
     STATIC_EQUAL(ct::ctcrc32("asdf"), ct::ctcrc32_ignore_whitespace("as   df"));
     STATIC_EQUAL(ct::ctcrc32Range("TestClass::functionName()", 0, ct::classNameIdx("TestClass::functionName()")), ct::ctcrc32("TestClass"));
+
+    STATIC_NE(ct::ctcrc32("asdfkjlaksjdfkjaslkdfjlkasjfklaslkfjsakdjflkasdfj"), 0);
 }
