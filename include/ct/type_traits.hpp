ab794233cdc0a2afb2c4794a66fc95c863cee000
dan <dtmoodie@gmail.com>
2019-06-19 01:41:02 -0400

    add remove_reference_t type trait

--- include/ct/type_traits.hpp
+++ include/ct/type_traits.hpp
@@ -146,6 +146,9 @@ namespace ct
     using decay_t = typename std::decay<T>::type;
 
     template <class T>
+    using remove_reference_t = typename std::remove_reference<T>::type;
+
+    template <class T>
     using remove_cv_t = typename std::remove_cv<T>::type;
 
     template <class T>
