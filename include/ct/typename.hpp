92784dd7951ae64d898e511bb52b09ee4e3b1b4c
dan <dtmoodie@gmail.com>
2019-07-27 18:39:27 -0400

    made it possible to specialize the name of types

--- include/ct/typename.hpp
+++ include/ct/typename.hpp
@@ -5,42 +5,68 @@
 
 namespace ct
 {
-namespace detail
-{
-    constexpr StringView parseClassNameGCC(const StringView name)
+    namespace detail
     {
-        return name.slice(name.rfind('=') + 2, name.size() - 1);
+        constexpr StringView parseClassNameGCC(const StringView name)
+        {
+            return name.slice(name.rfind('=') + 2, name.size() - 1);
+        }
+
+        constexpr StringView parseClassNameMSVC(const StringView name)
+        {
+            return name.slice(ct::findFirst(name.data(), ' ') + 1, ct::findLast(name.data(), '>'));
+        }
     }
 
-    constexpr StringView parseClassNameMSVC(const StringView name)
+    // This works at compile time on gcc 5.4 but not 4.8 :(
+    // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=55425
+    // Which is why CT_CONSTEXPR_NAME is constexpr on 5.4 and not 4.8
+    // It futher is then broken again until like gcc 9. :/
+    template <class T>
+    struct GetNameGCC
     {
-        return name.slice(ct::findFirst(name.data(), ' ') + 1, ct::findLast(name.data(), '>'));
-    }
-}
+        static CT_CONSTEXPR_NAME const char* funcName() { return CT_FUNCTION_NAME; }
+        static CT_CONSTEXPR_NAME StringView getName() { return detail::parseClassNameGCC(funcName()); }
+    };
 
-// This works at compile time on gcc 5.4 but not 4.8 :(
-// https://gcc.gnu.org/bugzilla/show_bug.cgi?id=55425
-// Which is why CT_CONSTEXPR_NAME is constexpr on 5.4 and not 4.8
-template <class T>
-struct GetNameGCC
-{
-    static CT_CONSTEXPR_NAME const char* funcName() { return CT_FUNCTION_NAME; }
-    static CT_CONSTEXPR_NAME StringView getName() { return detail::parseClassNameGCC(funcName()); }
-};
-
-template <class T>
-struct GetNameMSVC
-{
-    static constexpr StringView funcName() { return StringView(CT_FUNCTION_NAME); }
-    static constexpr StringView getName() { return detail::parseClassNameMSVC(funcName()); }
-};
+    template <class T>
+    struct GetNameMSVC
+    {
+        static constexpr StringView funcName() { return StringView(CT_FUNCTION_NAME); }
+        static constexpr StringView getName() { return detail::parseClassNameMSVC(funcName()); }
+    };
 #ifdef _MSC_VER
-template <class T>
-using GetName = GetNameMSVC<T>;
+    template <class T>
+    struct GetName : GetNameMSVC<T>
+    {
+    };
 #else
-template <class T>
-using GetName = GetNameGCC<T>;
+
+    template <class T>
+    struct GetName : GetNameGCC<T>
+    {
+    };
 #endif
 }
+
+#define DECL_NAME_1(name)                                                                                              \
+    template <>                                                                                                        \
+    struct GetName<name>                                                                                               \
+    {                                                                                                                  \
+        static constexpr StringView getName() { return #name; }                                                        \
+    }
+
+#define DECL_NAME_2(sym, name)                                                                                         \
+    template <>                                                                                                        \
+    struct GetName<sym>                                                                                                \
+    {                                                                                                                  \
+        static constexpr StringView getName() { return #name; }                                                        \
+    }
+
+#ifdef _MSC_VER
+#define DECL_NAME(...) CT_PP_CAT(CT_PP_OVERLOAD(DECL_NAME_, __VA_ARGS__)(__VA_ARGS__), CT_PP_EMPTY())
+#else
+#define DECL_NAME(...) CT_PP_OVERLOAD(DECL_NAME_, __VA_ARGS__)(__VA_ARGS__)
+#endif
 
 #endif // CT_TYPENAME_HPP
