7337364ac3ca0dbc6b7e5d69ba202588ae6125c4
dan <dtmoodie@gmail.com>
2019-08-30 21:24:27 -0400

    just disable this whole thing for msvc for now

--- tests/reflect/static_checks.cpp
+++ tests/reflect/static_checks.cpp
@@ -77,28 +77,28 @@ int main()
     ct::StaticEquality<ct::index_t, ct::Reflect<MultipleInheritance>::NUM_FIELDS, 6>();
     ct::StaticEquality<ct::index_t, ct::Reflect<MultipleInheritance>::START_INDEX, 5>();
 
-    if (ct::crc32(ct::Reflect<ReflectedStruct>:: getName()) != ct::crc32("ReflectedStruct"))
+    if (ct::crc32(ct::Reflect<ReflectedStruct>::getName()) != ct::crc32("ReflectedStruct"))
     {
         std::cout << "crc32 of ReflectedStruct not working" << std::endl;
     }
 
-    if (ct::crc32(ct::Reflect<Inherited>:: getName()) != ct::crc32("Inherited"))
+    if (ct::crc32(ct::Reflect<Inherited>::getName()) != ct::crc32("Inherited"))
     {
         std::cout << "crc32 of Inherited not working" << std::endl;
     }
 
-    if (ct::crc32(ct::Reflect<MultipleInheritance>:: getName()) != ct::crc32("MultipleInheritance"))
+    if (ct::crc32(ct::Reflect<MultipleInheritance>::getName()) != ct::crc32("MultipleInheritance"))
     {
         std::cout << "crc32 of MultipleInheritance not working" << std::endl;
     }
 #ifdef CT_HAVE_CONSTEXPR_NAME
-#if !(defined(_MSC_VER) && _MSC_VER == 1900)
-    ct::StaticEquality<uint32_t, ct::crc32(ct::Reflect<ReflectedStruct>:: getName()), ct::crc32("ReflectedStruct")>{};
+#if !(defined(_MSC_VER))
+    ct::StaticEquality<uint32_t, ct::crc32(ct::Reflect<ReflectedStruct>::getName()), ct::crc32("ReflectedStruct")>{};
 
-    ct::StaticEquality<uint32_t, ct::crc32(ct::Reflect<Inherited>:: getName()), ct::crc32("Inherited")>{};
+    ct::StaticEquality<uint32_t, ct::crc32(ct::Reflect<Inherited>::getName()), ct::crc32("Inherited")>{};
 
     ct::StaticEquality<uint32_t,
-                       ct::crc32(ct::Reflect<MultipleInheritance>:: getName()),
+                       ct::crc32(ct::Reflect<MultipleInheritance>::getName()),
                        ct::crc32("MultipleInheritance")>{};
 #endif
 #endif
