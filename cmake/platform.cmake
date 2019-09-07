9c2568685fe6fcbf25c3f159ded94cef7bfa47ee
dan <dtmoodie@gmail.com>
2019-09-06 23:19:54 -0400

    fix some clang interface stuff

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -7,11 +7,13 @@ function(setTargetPlatformFlags tgt)
             )
         else()
             # for gcc 4.8 we just set the c++1y flag to get return type deduction
-            target_compile_options(ct INTERFACE "-std=c++1y")
+            target_compile_options(${tgt} INTERFACE "-std=c++1y")
         endif()
 
     elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-        set(CMAKE_CXX_STANDARD 14)
-        set(CMAKE_CXX_STANDARD_REQUIRED ON)
+        target_compile_features(${tgt}
+            INTERFACE cxx_return_type_deduction
+        )
+        target_compile_options(${tgt} INTERFACE "-lstdc++")
     endif()
 endfunction()
