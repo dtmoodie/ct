6301934d4913037f2f86d670e1ca7135d9672bb5
dan <dtmoodie@gmail.com>
2019-09-16 23:21:15 -0400

    use cuda 6.5 since that's what my tk1 has and I'd like to test compat with that

--- scripts/travis/install-deps.sh
+++ scripts/travis/install-deps.sh
@@ -1,5 +1,5 @@
 apt-get install python-numpy -y
-CUDA_REPO_PKG=cuda-repo-ubuntu1404_7.5-18_amd64.deb
+CUDA_REPO_PKG=cuda-repo-ubuntu1404_6.5-19_amd64.deb
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/$CUDA_REPO_PKG
 dpkg -i $CUDA_REPO_PKG
 rm $CUDA_REPO_PKG
@@ -7,8 +7,8 @@ rm $CUDA_REPO_PKG
 apt-get -y update
 
 # install packages
-CUDA_PKG_VERSION="7-5"
-CUDA_VERSION="7.5"
+CUDA_PKG_VERSION="6-5"
+CUDA_VERSION="6.5"
 apt-get install -y --no-install-recommends \
 cuda-core-$CUDA_PKG_VERSION \
 cuda-cudart-dev-$CUDA_PKG_VERSION \
