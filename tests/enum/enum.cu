f11de8b143aa5c973cc296ec0d5c72854d1e17cc
dtmoodie <dtmoodie@gmail.com>
2019-12-21 14:08:29 -0500

    add test and workaround for issue 14 (#15)

--- tests/enum/enum.cu
+++ tests/enum/enum.cu
@@ -2,6 +2,8 @@
 #include "enum.hpp"
 #include <ct/enum.hpp>
 #include <iostream>
+#include <ct/static_asserts.hpp>
+
 ENUM_BEGIN(Test, uint32_t)
 
 static constexpr const EnumValueType k0 = 0;
@@ -17,4 +19,10 @@ int main()
     static_assert(MyClass::MyEnum::kVALUE0 == 1, "asdf");
     static_assert(MyClass::MyEnum::kVALUE1 == 2, "asdf");
     static_assert(MyClass::MyEnum::kVALUE2 == 3, "asdf");
+
+    ct::StaticEquality<uint64_t, MyClass::BitwiseEnum::kVALUE0, 1>{};
+    ct::StaticEquality<uint64_t, MyClass::BitwiseEnum::kVALUE1, (1 << 1)>{};
+    ct::StaticEquality<uint64_t, MyClass::BitwiseEnum::kVALUE2, (1 << 2)>{};
+    ct::StaticEquality<uint64_t, MyClass::ExtendedEnum::kVALUE3, (1 << 3)>{};
+    ct::StaticEquality<uint64_t, MyClass::ExtendedEnum::kVALUE4, (1 << 4)>{};
 }
