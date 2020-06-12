993bfed54a6a0b871cc632ed313948beebc702f1
dan <dtmoodie@gmail.com>
2020-06-12 13:27:38 -0400

    implement test ground truth for DataTableStoage

--- tests/reflect/print.cpp
+++ tests/reflect/print.cpp
@@ -62,6 +62,13 @@ EXPECTED_OBJECT_PRINT(std::vector<ReflectedStruct>,
                       "[(x: 0 y: 1 z: 2 id: 3 )  (x: 4 y: 5 z: 6 id: 7 )  (x: 8 y: 9 z: 10 id: 11 ) ]");
 EXPECTED_OBJECT_PRINT(StringMap, "{asdf:(x: 0 y: 1 z: 2 id: -1 w: 15 )  asdfg:(x: 0 y: 1 z: 4 id: -1 w: 15 ) }");
 
+EXPECTED_OBJECT_PRINT(ct::ext::DataTableStorage<float>, "(shape: [10 1] size: 10 data: [0 1 2 3 4 5 6 7 8 9] ) ");
+EXPECTED_OBJECT_PRINT(
+    ct::ext::DataTableStorage<ct::TArrayView<float>>,
+    "(shape: [10 10] size: 100 data: [0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 "
+    "30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 "
+    "68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99] ) ");
+
 #ifdef HAVE_OPENCV
 EXPECTED_OBJECT_PRINT(cv::Point2f, "(x: 0 y: 1 ) ");
 EXPECTED_OBJECT_PRINT(cv::Point, "(x: 0 y: 1 ) ");
