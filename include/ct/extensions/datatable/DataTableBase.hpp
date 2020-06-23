685a4749fb07e3f1412bb99615527f60f75dd481
asiansensation <dtmoodie@gmail.com>
2020-06-23 15:04:36 +0000

    one statement per line

--- include/ct/extensions/datatable/DataTableBase.hpp
+++ include/ct/extensions/datatable/DataTableBase.hpp
@@ -61,7 +61,8 @@ namespace ct
             template <index_t I>
             void reserveImpl(const size_t size, const ct::Indexer<I> idx)
             {
-                Storage::template get<I>().reserve(size);
+                auto& ref = Storage::template get<I>();
+                ref.reserve(size);
                 const auto next = --idx;
                 reserveImpl(size, next);
             }
@@ -69,14 +70,16 @@ namespace ct
             void push(const U& data, const ct::Indexer<0> idx)
             {
                 const auto accessor = Reflect<U>::getPtr(idx);
-                Storage::template get<0>().push_back(accessor.get(data));
+                auto& ref = Storage::template get<0>();
+                ref.push_back(accessor.get(data));
             }
 
             template <index_t I>
             void push(const U& data, const ct::Indexer<I> idx)
             {
                 const auto accessor = Reflect<U>::getPtr(idx);
-                Storage::template get<I>().push_back(accessor.get(data));
+                auto& ref = Storage::template get<I>();
+                ref.push_back(accessor.get(data));
                 const auto next = --idx;
                 push(data, next);
             }
@@ -85,7 +88,8 @@ namespace ct
             {
                 if (offset == m_field_offsets[0])
                 {
-                    return (Storage::template get<0>().data(index));
+                    auto& ref = Storage::template get<0>();
+                    return ref.data(index);
                 }
                 return {nullptr, 1};
             }
@@ -95,7 +99,8 @@ namespace ct
             {
                 if (offset == m_field_offsets[I])
                 {
-                    return (Storage::template get<I>().data(index));
+                    auto& ref = Storage::template get<I>();
+                    return ref.data(index);
                 }
                 const auto next = --field_index;
                 return ptr(offset, index, next);
@@ -111,7 +116,8 @@ namespace ct
             {
                 if (offset == m_field_offsets[0])
                 {
-                    return (Storage::template get<0>().data(index));
+                    auto& ref = Storage::template get<0>();
+                    return ref.data(index);
                 }
                 return {nullptr, 1};
             }
@@ -121,7 +127,8 @@ namespace ct
             {
                 if (offset == m_field_offsets[I])
                 {
-                    return (Storage::template get<I>().data(index));
+                    auto& ref = Storage::template get<I>();
+                    return ref.data(index);
                 }
                 const auto next = --field_index;
                 return ptr(offset, index, next);
@@ -136,8 +143,8 @@ namespace ct
             template <class T>
             const void* storageImpl(const size_t offset, ct::Indexer<0>) const
             {
-                if (std::is_same<typename std::decay<decltype(Storage::template get<0>())>::type, T>::value &&
-                    offset == m_field_offsets[0])
+                using storage_type = typename std::decay<decltype(Storage::template get<0>())>::type;
+                if (std::is_same<storage_type, T>::value && offset == m_field_offsets[0])
                 {
                     return &Storage::template get<0>();
                 }
@@ -147,8 +154,8 @@ namespace ct
             template <class T, index_t I>
             const void* storageImpl(const size_t offset, ct::Indexer<I> field_index) const
             {
-                if (std::is_same<typename std::decay<decltype(Storage::template get<I>())>::type, T>::value &&
-                    offset == m_field_offsets[I])
+                using storage_type = typename std::decay<decltype(Storage::template get<I>())>::type;
+                if (std::is_same<storage_type, T>::value && offset == m_field_offsets[I])
                 {
                     return &Storage::template get<I>();
                 }
@@ -159,8 +166,8 @@ namespace ct
             template <class T>
             void* storageImpl(const size_t offset, ct::Indexer<0>)
             {
-                if (std::is_same<typename std::decay<decltype(Storage::template get<0>())>::type, T>::value &&
-                    offset == m_field_offsets[0])
+                using storage_type = typename std::decay<decltype(Storage::template get<0>())>::type;
+                if (std::is_same<storage_type, T>::value && offset == m_field_offsets[0])
                 {
                     return &Storage::template get<0>();
                 }
@@ -170,8 +177,8 @@ namespace ct
             template <class T, index_t I>
             void* storageImpl(const size_t offset, ct::Indexer<I> field_index)
             {
-                if (std::is_same<typename std::decay<decltype(Storage::template get<I>())>::type, T>::value &&
-                    offset == m_field_offsets[I])
+                using storage_type = typename std::decay<decltype(Storage::template get<I>())>::type;
+                if (std::is_same<storage_type, T>::value && offset == m_field_offsets[I])
                 {
                     return &Storage::template get<I>();
                 }
