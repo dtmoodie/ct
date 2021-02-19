2ae2b9987477b0fef4b7a373724d22d6fbc0d1ff
dan <dtmoodie@gmail.com>
2021-02-18 20:55:50 -0500

    wip fixing search for boost python

---
+++ cmake/python.cmake
@@ -0,0 +1,10 @@
+find_package(Python COMPONENTS Interpreter Development)
+if(Python_FOUND)
+    if(${Python_VERSION} VERSION_GREATER_EQUAL 3.0.0)
+        include(${CMAKE_CURRENT_LIST_DIR}/python3.cmake)
+    else(${Python_VERSION} VERSION_GREATER_EQUAL 3.0.0)
+    include(${CMAKE_CURRENT_LIST_DIR}/python2.cmake)
+    endif(${Python_VERSION} VERSION_GREATER_EQUAL 3.0.0)
+else(Python_FOUND)
+
+endif(Python_FOUND)
