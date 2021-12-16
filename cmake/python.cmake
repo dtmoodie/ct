5cc837aa57746d4d385eb55772b494a82f58cd59
dan <dtmoodie@gmail.com>
2021-12-16 12:19:00 -0500

    improved cmake python search compatability

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -1,10 +1,10 @@
 find_package(Python COMPONENTS Interpreter Development NumPy)
+
 if(Python_FOUND)
+    message(STATUS "Found python ${Python_VERSION}")
     if(${Python_VERSION} VERSION_GREATER_EQUAL 3.0.0)
         include(${CMAKE_CURRENT_LIST_DIR}/python3.cmake)
     else(${Python_VERSION} VERSION_GREATER_EQUAL 3.0.0)
-    include(${CMAKE_CURRENT_LIST_DIR}/python2.cmake)
+        include(${CMAKE_CURRENT_LIST_DIR}/python2.cmake)
     endif(${Python_VERSION} VERSION_GREATER_EQUAL 3.0.0)
-else(Python_FOUND)
-
 endif(Python_FOUND)
