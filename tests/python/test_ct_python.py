330b42dd8eb96019b5cc387ed572c357168c55d1
dan <dtmoodie@gmail.com>
2026-05-30 10:22:25 -0400

    update python tests

--- tests/python/test_ct_python.py
+++ tests/python/test_ct_python.py
@@ -1,6 +1,20 @@
-import imp
+import importlib
 import os
+
 if os.path.exists('libtest_ct_python.so'):
-    imp.load_dynamic('test_ct_python','libtest_ct_python.so')
+    filename = 'libtest_ct_python.so'
 else:
-    imp.load_dynamic('test_ct_python','libtest_ct_pythond.so')
+    filename = 'libtest_ct_pythond.so'
+    
+spec = importlib.util.spec_from_file_location('test_ct_python', filename)
+mod = importlib.util.module_from_spec(spec)
+
+A = mod.DerivedA()
+assert A.base_x == 0
+assert A.base_y == 0
+assert A.base_z == 0
+assert A.derived_a == 0
+
+B = mod.DerivedB()
+
+print("Success")
