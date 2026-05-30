ba765d025ba449de5376a4aecd684100708f7660
dan <dtmoodie@gmail.com>
2026-05-30 10:39:43 -0400

    cleanup imort file

--- tests/python/test_ct_python.py
+++ tests/python/test_ct_python.py
@@ -1,20 +1,11 @@
 import importlib
 import os
 
-if os.path.exists('libtest_ct_python.so'):
-    filename = 'libtest_ct_python.so'
+
+if os.path.exists('tests/python/libtest_ct_python.so'):
+    filename = 'tests/python/libtest_ct_python.so'
 else:
-    filename = 'libtest_ct_pythond.so'
-    
+    filename = 'tests/python/libtest_ct_pythond.so'
+
 spec = importlib.util.spec_from_file_location('test_ct_python', filename)
 mod = importlib.util.module_from_spec(spec)
-
-A = mod.DerivedA()
-assert A.base_x == 0
-assert A.base_y == 0
-assert A.base_z == 0
-assert A.derived_a == 0
-
-B = mod.DerivedB()
-
-print("Success")
